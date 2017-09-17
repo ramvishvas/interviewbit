6aee697bb732f62fdc36beac13bdab3a2f9f4b01
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-09-17 16:41:47 +0000

    Update MinStepsInInfiniteGrid.java

--- Array/MinStepsInInfiniteGrid.java
+++ Array/MinStepsInInfiniteGrid.java
@@ -1,20 +1,35 @@
-You are in an infinite 2D grid where you can move in any of the 8 directions :
+// You are in an infinite 2D grid where you can move in any of the 8 directions :
 
- (x,y) to 
-    (x+1, y), 
-    (x - 1, y), 
-    (x, y+1), 
-    (x, y-1), 
-    (x-1, y-1), 
-    (x+1,y+1), 
-    (x-1,y+1), 
-    (x+1,y-1) 
+//  (x,y) to 
+//     (x+1, y), 
+//     (x - 1, y), 
+//     (x, y+1), 
+//     (x, y-1), 
+//     (x-1, y-1), 
+//     (x+1,y+1), 
+//     (x-1,y+1), 
+//     (x+1,y-1) 
 
-You are given a sequence of points and the order in which you need to cover the points. Give the minimum number of steps in which you can achieve it. You start from the first point.
+// You are given a sequence of points and the order in which you need to cover the points. Give the minimum number of steps in which you can achieve it. You start from the first point.
 
-Example :
+// Example :
 
-Input : [(0, 0), (1, 1), (1, 2)]
-Output : 2
+// Input : [(0, 0), (1, 1), (1, 2)]
+// Output : 2
 
-It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+// It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
+
+public class Solution {
+    // X and Y co-ordinates of the points in order.
+    // Each point is represented by (X.get(i), Y.get(i))
+    public int coverPoints(ArrayList<Integer> X, ArrayList<Integer> Y) {
+        int step = 0;
+        for(int i = 1; i < X.size(); i++)
+        {
+            step += Math.max(Math.abs(X.get(i) - X.get(i-1)), Math.abs(Y.get(i) - Y.get(i-1)));
+        }
+        
+        return step;
+    }
+}
+
