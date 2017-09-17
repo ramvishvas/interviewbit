927b05ee655065670f66c8b20967e2ab86f1e187
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-09-17 16:50:13 +0000

    Update RepeatAndMissingNumberArray.java

--- Array/RepeatAndMissingNumberArray.java
+++ Array/RepeatAndMissingNumberArray.java
@@ -1,31 +1,87 @@
-Please Note:
+// Please Note:
 
-There are certain problems which are asked in the interview to also check how you take care of overflows in your problem.
-This is one of those problems.
-Please take extra care to make sure that you are type-casting your ints to long properly and at all places. Try to verify if your solution works if number of elements is as large as 105
+// There are certain problems which are asked in the interview to also check how you take care of overflows in your problem.
+// This is one of those problems.
+// Please take extra care to make sure that you are type-casting your ints to long properly and at all places. Try to verify if your solution works if number of elements is as large as 105
 
-    Food for thought :
+//     Food for thought :
 
-        Even though it might not be required in this problem, in some cases, you might be required to order the operations cleverly so that the numbers do not overflow.
-        For example, if you need to calculate n! / k! where n! is factorial(n), one approach is to calculate factorial(n), factorial(k) and then divide them.
-        Another approach is to only multiple numbers from k + 1 ... n to calculate the result.
-        Obviously approach 1 is more susceptible to overflows.
+//         Even though it might not be required in this problem, in some cases, you might be required to order the operations cleverly so that the numbers do not overflow.
+//         For example, if you need to calculate n! / k! where n! is factorial(n), one approach is to calculate factorial(n), factorial(k) and then divide them.
+//         Another approach is to only multiple numbers from k + 1 ... n to calculate the result.
+//         Obviously approach 1 is more susceptible to overflows.
 
-You are given a read only array of n integers from 1 to n.
+// You are given a read only array of n integers from 1 to n.
 
-Each integer appears exactly once except A which appears twice and B which is missing.
+// Each integer appears exactly once except A which appears twice and B which is missing.
 
-Return A and B.
+// Return A and B.
 
-Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
-Note that in your output A should precede B.
+// Note that in your output A should precede B.
 
-Example:
+// Example:
 
-Input:[3 1 2 5 3] 
+// Input:[3 1 2 5 3] 
 
-Output:[3, 4] 
+// Output:[3, 4] 
 
-A = 3, B = 4
+// A = 3, B = 4
+
+// public class Solution {
+// 	// DO NOT MODIFY THE LIST
+// 	public ArrayList<Integer> repeatedNumber(final List<Integer> a) {
+	    
+// 	    ArrayList<Integer> sol = new ArrayList<Integer>(2);
+// 	    sol.add(0);
+// 	    sol.add(1);
+// 	    for(int i = 0; i < a.size(); i++)
+// 	    {
+// 	        if(a.contains(i+1))
+// 	        {
+// 	            if(a.indexOf(i+1) != a.lastIndexOf(i+1))
+// 	            {
+// 	                sol.set(0,i+1);
+// 	            }
+// 	        }
+// 	        else
+// 	        {
+// 	           sol.set(1,i+1);
+// 	        }
+	        
+// 	    }
+// 	    return sol;
+// 	}
+// }
+
+
+public class Solution {
+	// DO NOT MODIFY THE LIST
+	public ArrayList<Integer> repeatedNumber(final List<Integer> A) {
+	    
+	    ArrayList<Integer> res = new ArrayList<>();
+	    
+	    Collections.sort(A);
+	    int n = A.size();
+	    int rep = -1;
+	    int miss = -1;
+	    long sum = A.get(0);
+	    
+	    for (int i = 1; i < n; i++) {
+	        if (A.get(i).intValue() == A.get(i - 1).intValue()) {
+	            rep = A.get(i);
+	        }
+            sum += A.get(i);
+	    }
+	    
+	    miss = (int) ((1L * n * (1L * n + 1)) / 2 - sum + rep);
+	    
+	    res.add(rep);
+	    res.add(miss);
+	    
+	    return res;
+	    
+	}
+}
 
