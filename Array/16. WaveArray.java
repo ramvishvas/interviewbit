a55b2186c293685c0730ca9a4d5169155292b416
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-09-23 12:07:05 +0530

    Update 16. WaveArray.java

--- Array/16. WaveArray.java
+++ Array/16. WaveArray.java
@@ -1,1 +1,37 @@
+// Given an array of integers, sort the array into a wave like array and return it, 
+// In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
 
+// Example
+
+// Given [1, 2, 3, 4]
+
+// One possible answer : [2, 1, 4, 3]
+// Another possible answer : [4, 1, 3, 2]
+//  NOTE : If there are multiple answers possible, return the one thats lexicographically smallest. 
+// So, in example case, you will return [2, 1, 4, 3]
+
+public class Solution {
+	public ArrayList<Integer> wave(ArrayList<Integer> a) {
+	   // for(int i = 1; i < a.size(); i += 2){
+	   //     if(a.get(i - 1) < a.get(i)){
+	   //         int temp = a.get(i);
+	   //         a.set(i, a.get(i-1));
+	   //         a.set(i-1, temp);
+	   //     }
+	   //     if(i != (a.size() - 1)){
+    // 	        if(a.get(i + 1) < a.get(i)){
+    // 	            int temp = a.get(i+1);
+    // 	            a.set(i+1, a.get(i));
+    // 	            a.set(i, temp);
+    // 	        }
+	   //     }
+	   // }
+	   Collections.sort(a);
+	    for(int i = 1; i < a.size(); i += 2){
+	        int temp = a.get(i);
+            a.set(i, a.get(i-1));
+            a.set(i-1, temp);
+	   }
+	    return a;
+	}
+}
