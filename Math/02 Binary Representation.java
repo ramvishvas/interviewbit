d7669a2fc838c30d039366430f57c96dabf4dd59
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-10-10 16:49:22 +0530

    Update 02 Binary Representation.java

--- Math/02 Binary Representation.java
+++ Math/02 Binary Representation.java
@@ -1,1 +1,21 @@
 
+// Given a number N >= 0, find its representation in binary.
+
+// Example:
+
+// if N = 6,
+
+// binary form = 110
+
+
+public class Solution {
+	public String findDigitsInBinary(int a) {
+	    String str = Integer.toString(a%2);
+	    a = a/2;
+	    while(a > 0){
+	        str = Integer.toString(a%2) + str;
+	        a /= 2;
+	    }
+	    return str;
+	}
+}
