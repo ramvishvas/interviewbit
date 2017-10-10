18eb40377e56ca0af49f37b9774e76e0b64fa67a
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-10-10 16:52:59 +0530

    Update 04 Verify Prime.java

--- Math/04 Verify Prime.java
+++ Math/04 Verify Prime.java
@@ -1,8 +1,29 @@
-Given a number N, verify if N is prime or not.
+// Given a number N, verify if N is prime or not.
 
-Return 1 if N is prime, else return 0.
+// Return 1 if N is prime, else return 0.
 
-Example :
+// Example :
 
-Input : 7
-Output : True
+// Input : 7
+// Output : True
+
+
+public class Solution {
+	public int isPrime(int a) {
+	    if(a == 1){
+	       return 0;
+	    }
+	    else if(a == 2 || a == 3){
+	       return 1;
+	    }
+	    else if(a % 2 == 0){
+	       return 0;
+	    }
+	    for(int i = 3; i <= Math.sqrt(a); i += 2){
+	        if(a % i == 0){
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+}
