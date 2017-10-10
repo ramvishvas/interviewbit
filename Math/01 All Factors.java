e9d65d02b05fda864465b38a9130cccd93cf0f4d
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-10-10 16:47:31 +0530

    Update 01 All Factors.java

--- Math/01 All Factors.java
+++ Math/01 All Factors.java
@@ -1,7 +1,28 @@
-Given a number N, find all factors of N.
+// Given a number N, find all factors of N.
 
-Example:
+// Example:
 
-N = 6 
-factors = {1, 2, 3, 6}
-Make sure the returned array is sorted.
+// N = 6 
+// factors = {1, 2, 3, 6}
+// Make sure the returned array is sorted.
+
+
+public class Solution {
+	public ArrayList<Integer> allFactors(int a) {
+	    ArrayList<Integer> ans = new ArrayList<Integer>();
+	    if(a == 1){
+	        ans.add(a);
+	        return ans;
+	    }
+	    for(int i = 1; i <= Math.sqrt(a); i++){
+	        if((a % i) == 0){
+	            ans.add(i);
+	            if(i != Math.sqrt(a)){
+	                ans.add(a/i);
+	            }
+	        }
+	    }
+	    Collections.sort(ans);
+	    return ans;
+	}
+}
