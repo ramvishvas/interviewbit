632afd24f8966cc6f95cfe453df3537899a46c47
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-10-10 16:50:58 +0530

    Update 03 Prime Numbers.java

--- Math/03 Prime Numbers.java
+++ Math/03 Prime Numbers.java
@@ -1,8 +1,34 @@
-Given a number N, verify if N is prime or not.
+// Given a number N, find all prime numbers upto N ( N included ).
 
-Return 1 if N is prime, else return 0.
+// Example:
 
-Example :
+// if N = 7,
 
-Input : 7
-Output : True
+// all primes till 7 = {2, 3, 5, 7}
+
+// Make sure the returned array is sorted.
+
+public class Solution {
+	public ArrayList<Integer> sieve(int a) {
+	    ArrayList<Integer> ans = new ArrayList();
+	    int[] arr = new int[a + 1];
+	    for(int i = 0; i <= a; i++){
+	        arr[i] = 1;
+	    }
+	    arr[0] = 0;
+	    arr[1] = 0;
+	    for(int i = 2; i <= Math.sqrt(a); i++){
+            int j = 2;
+            while(j*i <= a){
+                arr[j*i] = 0;
+                j++;
+            }
+	    }
+	    for(int i = 2; i <= a; i++){
+	        if(arr[i] == 1){
+	            ans.add(i);
+	        }
+	    }
+	    return ans;
+	}
+}
