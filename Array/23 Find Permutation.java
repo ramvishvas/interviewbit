f2e42ebc1071e3b3ad569c5e8453a357caecf279
Ramvishvas Kumar <ramvishvasg@gmail.com>
2017-10-22 16:18:58 +0530

    Update 23 Find Permutation.java

--- Array/23 Find Permutation.java
+++ Array/23 Find Permutation.java
@@ -1,17 +1,48 @@
-Given a positive integer n and a string s consisting only of letters D or I, you have to find any permutation of first n positive integer that satisfy the given input string.
+// Given a positive integer n and a string s consisting only of letters D or I, you have to find any permutation of first n positive integer that satisfy the given input string.
 
-D means the next number is smaller, while I means the next number is greater.
+// D means the next number is smaller, while I means the next number is greater.
 
-Notes
+// Notes
 
-Length of given string s will always equal to n - 1
-Your solution should run in linear time and space.
-Example :
+// Length of given string s will always equal to n - 1
+// Your solution should run in linear time and space.
+// Example :
 
-Input 1:
+// Input 1:
 
-n = 3
+// n = 3
 
-s = ID
+// s = ID
 
-Return: [1, 3, 2]
+// Return: [1, 3, 2]
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public ArrayList<Integer> findPerm(final String A, int B) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        // given B = a.length()-1
+        // Algorithm is simple
+        // whenever you encounter an I
+        // pop all element and add it to ans;
+        // and if you encounter an D add it to stack.
+        // examle 1 2 3 4 5 6 7 8 9 10
+        // string I D D D I I D I D
+        for (int i = 1; i < B; i++) {
+            if (A.charAt(i - 1) == 'I') {
+                ans.add(i);
+                while (!stack.isEmpty()){
+                    ans.add(stack.pop());   
+                }
+            }
+            else{
+                stack.push(i); 
+            }
+        }
+        
+        ans.add(B);
+        while (!stack.isEmpty()){
+            ans.add(stack.pop());   
+        }
+        return ans;
+    }
+}
